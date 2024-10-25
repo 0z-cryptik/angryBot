eae3acb7aab4c592ba13e21e52673e64ad2234e2
Eniitan <enitanolawale0@gmail.com>
2024-10-26 00:42:58 +0100

    foundations laid

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("fuck 12")
+import { config } from "dotenv";
+import TelegramBot, { Message } from "node-telegram-bot-api"; // Import both TelegramBot and Message types
+
+config();
+
+const botKey: string = process.env.BOT_KEY
+
+const bot = new TelegramBot(botKey, { polling: true });
+
+// Use the Message type for the msg parameter
+bot.on("message", (msg: Message) => {
+    const chatId = msg.chat.id;
+    const messageText = msg.text;
+    const userName = msg.chat.first_name
+    console.log(`Received message from chatId ${chatId}: ${messageText}`);
+
+    if (messageText === "/start"){
+        bot.sendMessage(chatId, `Burger off ${userName}`)
+    }
+});
