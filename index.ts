4a5e2d6c4321ac6227ee9cf890a1307fb897d4cc
Eniitan <enitanolawale0@gmail.com>
2024-10-27 00:52:13 +0100

    some configuration for vercel deployment

--- index.ts
+++ index.ts
@@ -1,19 +1,39 @@
 import { config } from "dotenv";
-import TelegramBot, { Message } from "node-telegram-bot-api";
+import { Message } from "node-telegram-bot-api";
+import express, { Express, Request, Response } from "express";
 
 config();
 
 const botKey: string = process.env.BOT_KEY;
+const app: Express = express();
+const port = process.env.PORT;
 
-const bot = new TelegramBot(botKey, { polling: true });
+app.use(express.json());
 
-bot.on("message", (msg: Message) => {
+app.post("/webhook", async (req: Request, res: Response) => {
+  console.log(req.body);
+
+  const msg: Message = req.body;
+
   const chatId = msg.chat.id;
-  const messageText = msg.text;
-  const userName = msg.chat.first_name;
-  console.log(`Received message from chatId ${chatId}: ${messageText}`);
+  //const messageText = msg.text;
 
-  if (messageText === "/start") {
-    bot.sendMessage(chatId, `Burger off ${userName}`);
+  try {
+    await fetch(`https://api.telegram.org/bot${botKey}/sendMessage`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: "hey"
+      })
+    });
+  } catch (err) {
+    console.error(err);
   }
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
