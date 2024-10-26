56efe0f479cb18a7e4ce6f57b7ff5ee5185e1e32
Eniitan <enitanolawale0@gmail.com>
2024-10-26 23:10:40 +0100

    test version done

--- index.ts
+++ index.ts
@@ -1,20 +1,19 @@
 import { config } from "dotenv";
-import TelegramBot, { Message } from "node-telegram-bot-api"; // Import both TelegramBot and Message types
+import TelegramBot, { Message } from "node-telegram-bot-api";
 
 config();
 
-const botKey: string = process.env.BOT_KEY
+const botKey: string = process.env.BOT_KEY;
 
 const bot = new TelegramBot(botKey, { polling: true });
 
-// Use the Message type for the msg parameter
 bot.on("message", (msg: Message) => {
-    const chatId = msg.chat.id;
-    const messageText = msg.text;
-    const userName = msg.chat.first_name
-    console.log(`Received message from chatId ${chatId}: ${messageText}`);
+  const chatId = msg.chat.id;
+  const messageText = msg.text;
+  const userName = msg.chat.first_name;
+  console.log(`Received message from chatId ${chatId}: ${messageText}`);
 
-    if (messageText === "/start"){
-        bot.sendMessage(chatId, `Burger off ${userName}`)
-    }
+  if (messageText === "/start") {
+    bot.sendMessage(chatId, `Burger off ${userName}`);
+  }
 });
